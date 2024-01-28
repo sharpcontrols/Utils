2fa7de399d1e6216378c0ec3d29358b810b712d1
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2024-01-28 17:47:00 -0300

    Create Blogging.cs

---
+++ Blogging.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class Blogging
+    {
+        /// <summary>
+        /// Calculates the estimated reading time of a text in minutes, based on the words per minute
+        /// </summary>
+        /// <param name="text">The text to read</param>
+        /// <param name="wordsPerMinute">The estimated times of word read per minute</param>
+        /// <returns>Estimated reading time in rounded minutes</returns>
+        public static int EstimatedReadingTime(string text, int wordsPerMinute = 250)
+        {
+            int wordsCount = text.Length;
+            return (int)MathF.Round(wordsCount / wordsPerMinute);
+        }
+    }
+}
